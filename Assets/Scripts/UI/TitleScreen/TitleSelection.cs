913232247dcf7782af06de18f2ef3084cbcb85aa
Tyler Hurdle <Headspce>
2025-01-05 17:43:11 -0800

    Added_Logic_for_Entering_Overworld_and_Quitting_from_Title_Screen

--- Assets/Scripts/UI/TitleScreen/TitleSelection.cs
+++ Assets/Scripts/UI/TitleScreen/TitleSelection.cs
@@ -1,9 +1,14 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleSelection : MonoBehaviour
 {
     public GameObject startScreen;
     public GameObject quitScreen;
+    public string LevelName;
 
     void Update()
     {
@@ -11,11 +16,34 @@ public class TitleSelection : MonoBehaviour
         {
             startScreen.SetActive(true);
             quitScreen.SetActive(false);
+            Debug.Log("Start screen selected");
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             startScreen.SetActive(false);
             quitScreen.SetActive(true);
+            Debug.Log("Quit screen selected");
+        }
+
+        // Check if the Enter key is pressed
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (startScreen.activeSelf)
+            {
+                // Load the new level when startScreen is active and Enter is pressed
+                Debug.Log("Loading new level: " + LevelName);
+                SceneManager.LoadScene(LevelName);
+            }
+            else if (quitScreen.activeSelf)
+            {
+                // Quit the application when quitScreen is active and Enter is pressed
+                Debug.Log("Quitting application");
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            }
         }
     }
 }
